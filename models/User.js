025d247c071df667b7271dfe033d2776dcf69bc3
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-15 13:26:44 -0700

    Created basic schema for User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose');
+
+// defining the User Schema
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+        // TODO: ADD EMAIL VALIDATION HERE
+    },
+    // thoughts: array of _id values referencing the Thought model
+    // friends: array of _id values referencing the User model (self referencing)
+});
+
+// TODO: Create virtual called friendCount that retrieves the length of the user's friends array field on query
+
+module.exports;
