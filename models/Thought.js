12c44dbbc64a50a97d584c6fe62a9cc4bbf158d3
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-15 13:32:11 -0700

    Created basic schema for Thought Model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,24 @@
 const mongoose = require('mongoose');
 
+// defining the Thought Schema
+const thoughtSchema = mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        // TODO: MUST BE 1-280 CHARACTERS
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // TODO: USE A GETTER METHOD TO FORMAT TIMESTAMP ON QUERY
+    },
+    // USERNAME THAT CREATED THIS THOUGHT
+    username: {
+        type: String,
+        required: true
+    }
+    // SIMILAR TO REPLIES
+    // reactions: { ARRAY OF NESTED DOCUMENTS CREATED W reactionSchema }
+});
 
 module.exports;
