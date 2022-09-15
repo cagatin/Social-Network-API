aebd0b9915d128df552dd8884683b45c981a5447
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-15 13:46:54 -0700

    Added middlewear to server, start server and database

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middlewear
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Start the server and open the connection
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
