0c38afe9cddeb9b162ab686f87d9217d0a77a9a2
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-17 15:03:44 -0700

    Included virtuals of Thought Schema to JSON response

--- models/Thought.js
+++ models/Thought.js
@@ -1,28 +1,36 @@
 const mongoose = require('mongoose');
 
 // defining the Thought Schema
-const thoughtSchema = mongoose.Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        min: 1,
-        max: 280
+const thoughtSchema = mongoose.Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // TODO: USE A GETTER METHOD TO FORMAT TIMESTAMP ON QUERY
+        },
+        // USERNAME THAT CREATED THIS THOUGHT
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Reaction'
+        }]
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        // TODO: USE A GETTER METHOD TO FORMAT TIMESTAMP ON QUERY
-    },
-    // USERNAME THAT CREATED THIS THOUGHT
-    username: {
-        type: String,
-        required: true
-    },
-    reactions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Reaction'
-    }]
-});
+    {
+        //include virtuals in our JSON response
+        toJson: {
+            virtuals: true
+        }
+    }
+);
 
 //reactionCount virtual that retrieves the length of the thought's reactions array field on query
 thoughtSchema
