09aa5e3c73a9f90bdf19dcdc3d651c43c0753f3b
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-17 14:57:58 -0700

    Created email validation function

---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+const emailValidation = (email) => {
+    const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return regex.test(email);
+}
+
+module.exports = {
+    emailValidation
+};
