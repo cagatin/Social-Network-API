fc6c0eb2ba057fbeee91501d396e19243f370f02
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-17 15:23:37 -0700

    Created dateFormatter helper function using moment package

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,15 @@
+const moment = require('moment');
+
+// Validates an email string using regex
 const emailValidation = (email) => {
     const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
     return regex.test(email);
 }
 
+// Formats object in YYYY-MM-DD Format
+const dateFormatter = (date) => moment(date).format('YYYY-MM-DD');
+
 module.exports = {
-    emailValidation
+    emailValidation,
+    dateFormatter
 };
