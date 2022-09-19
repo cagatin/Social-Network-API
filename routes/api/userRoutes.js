43a1e400f980c1ce60a5a7e69cdb1f6b88a46cac
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-19 14:40:40 -0700

    Removed userRoutes from index.js file, placed into own userRoutes file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,19 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../controllers/userController');
+
+// api/users
+router.route('/')
+    .get(getUsers)
+    .get(getSingleUser)
+    .post(createUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// export the module
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const {
-    getUsers,
-    getSingleUser,
-    createUser,
-    updateUser,
-    deleteUser
-} = require('../controllers/userController');
 
-// api/users
-router.route('/')
-    .get(getUsers)
-    .get(getSingleUser)
-    .post(createUser)
-    .put(updateUser)
-    .delete(deleteUser);
 
 // export the module
 module.exports = router;
