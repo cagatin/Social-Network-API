005c89f2ec9230848aa621afbe47ebabbb3997dc
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-19 14:48:26 -0700

    Added proper paths to the user/thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const thoughtRoutes;
+const thoughtRoutes = require('./thoughtRoutes');
+
+// Attatch the user/thought paths to the routes.
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
