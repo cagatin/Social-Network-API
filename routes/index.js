64b23a3dcab56b31af146d82e594fce91c394071
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-19 14:51:31 -0700

    Attatched /api to the paths of the routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// attatch /api to the beginning out the paths
+router.use('/api', apiRoutes);
+
+// Error handler for wrong routes
+router.use((req, res) => {
+    return res.send('Invalid Route!');
+})
 
 // export the module
 module.exports = router;
