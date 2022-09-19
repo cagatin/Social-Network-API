151b86dbbbac3fe4d289fcfbda0779c52e21012b
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-19 09:55:52 -0700

    Created controller for getting all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,14 @@
-const User = require('../')
+const User = require('../models/User');
+
+const getUsers = function (req, res) {
+    try {
+        const userData = User.find();
+        if (!userData) {
+            res.status(404).json({ message: "No users found in the database!" });
+        }
+        res.status(200).json(userData);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+}
--- utils/dateFormatter.js
+++ utils/dateFormatter.js
@@ -1,6 +1,5 @@
 const moment = require('moment');
 
-
 // Formats object in YYYY-MM-DD Format
 const dateFormatter = (date) => moment(date).format('YYYY-MM-DD');
 
