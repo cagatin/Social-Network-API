d5f9bc52331acaecb2b2a67720172ea9854a6d4f
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-19 14:39:14 -0700

    Created routes for user

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const userRoutes;
+const thoughtRoutes;
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../controllers/userController');
+
+// api/users
+router.route('/')
+    .get(getUsers)
+    .get(getSingleUser)
+    .post(createUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// export the module
+module.exports = router;
