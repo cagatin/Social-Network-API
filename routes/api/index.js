3e8c07b2e2a0737e7eee9ad39c054afad233c58c
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-19 14:46:35 -0700

    Created api routes for Thought

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const thoughtRoutes;
+const thoughtRoutes;
 
 module.exports = router;
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,3 +8,19 @@ const {
     createReaction,
     deleteReaction
 } = require('../../controllers/thoughtController');
+
+// api/thought/
+router.route('/')
+    .get(getThoughts)
+    .get(getSingleThought)
+    .post(createThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// api/thoughts/:thoughtId/reactions/:reactionsId
+router.route(':thoughtId/reactions/:reactionsId').delete(deleteReaction);
+
+module.exports = router;
