cb9fb2d7476d6a1c62a5dc8dd2888f5ff10e4525
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-19 14:42:23 -0700

    Fixed import statement for User controller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userRoutes;
-const thoughtRoutes;
+const userRoutes = require('./userRoutes');
+// const thoughtRoutes;
 
 module.exports = router;
